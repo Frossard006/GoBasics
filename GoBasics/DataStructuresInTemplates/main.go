e71f8b0ddfa4996a3a50ddebd241c27cb0dd07ef
Matheus Frossard <matheus.frossard@hotmail.com>
2021-12-16 10:50:43 -0300

    add templates study

---
+++ GoBasics/DataStructuresInTemplates/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+var tpl *template.Template
+
+type sage struct {
+	Name  string
+	Motto string
+}
+
+type car struct {
+	Manufacturer string
+	Model        string
+	Doors        int
+}
+
+func init() {
+	tpl = template.Must(template.ParseFiles("one.gohtml"))
+}
+
+func main() {
+
+	http.HandleFunc("/", handle)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	b := sage{
+		Name:  "Buddha",
+		Motto: "The belief of no beliefs",
+	}
+
+	g := sage{
+		Name:  "Gandhi",
+		Motto: "Be the change",
+	}
+
+	m := sage{
+		Name:  "Martin Luther King",
+		Motto: "Hatred never ceases with hatred but with love alone is healed.",
+	}
+
+	f := car{
+		Manufacturer: "Ford",
+		Model:        "F150",
+		Doors:        2,
+	}
+
+	c := car{
+		Manufacturer: "Toyota",
+		Model:        "Corolla",
+		Doors:        4,
+	}
+
+	sages := []sage{b, g, m}
+	cars := []car{f, c}
+
+	data := struct {
+		Wisdom    []sage
+		Transport []car
+	}{
+		sages,
+		cars,
+	}
+	tpl.ExecuteTemplate(w, "one.gohtml", data)
+}
